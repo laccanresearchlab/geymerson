7afb3735ed972b37862b726d6e1f0dacb479b2c2
geymerson <geymerson.r@gmail.com>
2017-02-15 21:26:50 -0300

    arduino node, Sense.java

--- arduino/node/node.ino
+++ arduino/node/node.ino
@@ -1,8 +1,6 @@
-
-/* A application for arduino sensing nodes
-   this application uses series 2 XBEEs to wirelessly
+/* An application for arduino sensing nodes.
+   This application uses S2 XBEEs to wirelessly
    send and receive data
-
    @author: Geymerson Ramos <geymerson.r@gmail.com>
 */
 
@@ -13,41 +11,36 @@
 //Setting up XBEE
 XBee xbee = XBee();
 XBeeResponse response = XBeeResponse();
-
-// create reusable response objects for responses we expect to handle
 ZBRxResponse rx = ZBRxResponse();
 ModemStatusResponse msr = ModemStatusResponse();
 
 int lights = 8;
 
 void setup() {
-  //Set serial ports
+  //Setup leds
+  pinMode(lights,  OUTPUT);
+
+  // start serial
   Serial.begin(9600);
   xbee.begin(Serial);
-  pinMode(lights, OUTPUT);
 }
 
 void loop() {
 
-  /***************** A simple  test *****************/
-
-  delay(3000);
-  digitalWrite(lights, !digitalRead(lights));
-
-  /**************************************************/
-
-
-   //TODO
-   
-  //Keep waiting for packets
   xbee.readPacket();
 
-  //If a packet arrived
   if (xbee.getResponse().isAvailable()) {
     xbee.getResponse().getZBRxResponse(rx);
-  }
+    char command = (char)rx.getData(0);
 
-  else if (xbee.getResponse().isError()) {
-    //TODO
+    if (command == '0') {
+      digitalWrite(lights, LOW);
+      Serial.print(command);
+    }
+    else if (command == '1') {
+      Serial.print(command);
+      digitalWrite(lights, HIGH);
+    }
+    Serial.println();
   }
 }
--- senseApp/src/main/java/com/laccan/senseApp/Sense.java
+++ senseApp/src/main/java/com/laccan/senseApp/Sense.java
@@ -17,31 +17,34 @@ import net.tinyos.util.PrintStreamMessenger;
 import com.digi.xbee.api.DigiMeshDevice;
 import com.digi.xbee.api.RemoteXBeeDevice;
 import com.digi.xbee.api.XBeeNetwork;
-//import com.digi.xbee.api.ZigBeeDevice;
 import com.digi.xbee.api.exceptions.XBeeException;
 import com.digi.xbee.api.utils.HexUtils;
 
 class Sense implements MessageListener {
-	
+
 	/**************** MICAz's constants ****************/
-	
+
 	private PhoenixSource phoenix;
 	private MoteIF mif;
 	private String[] environments = {"lab_15", "lab_16", "lab_17", 
-										"prof_1", "prof_2", "prof_3",
-										"prof_4", "prof_5", "non","reun"};
+			"prof_1", "prof_2", "prof_3",
+			"prof_4", "prof_5", "non","reun"};
 	private SimpleDateFormat dt;
 	private String date;
-	
+
 	/**************** XBEE's constants ****************/
 	//Replace with the serial port where your receiver module is connected.
 	private static final String PORT = "/dev/ttyUSB2";
 	//Replace with the baud rate of you receiver module.
 	private static final int BAUD_RATE = 9600;
-	
-	private static final String DATA_TO_SEND = "Hello XBee!";
-	private static final String REMOTE_NODE_IDENTIFIER = "R2";
-	
+
+	private static final String DATA_TO_SEND = "1";
+	private static final String DATA_TO_SEND1 = "0";
+	private static final String REMOTE_NODE_IDENTIFIER = "env_16_tem01";
+
+	//Indicate if the light is on or off
+	private boolean lightsOn = false;
+
 	// Examples of endpoints, cluster ID and profile ID.
 	private static final int SOURCE_ENDPOINT = 0xA0;
 	private static final int DESTINATION_ENDPOINT = 0xA1;
@@ -53,7 +56,7 @@ class Sense implements MessageListener {
 		phoenix = BuildSource.makePhoenix(source, PrintStreamMessenger.err);
 		mif = new MoteIF(phoenix);
 		mif.registerListener(new SenseMsg(),this);
-//		Example "2016-06-24T21:58:19.000Z"
+		//		Example "2016-06-24T21:58:19.000Z"
 		dt = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.000Z");			
 	}
 
@@ -90,6 +93,46 @@ class Sense implements MessageListener {
 		converted[1]=(-2.0468+0.0367*(double)Humidity-0.0000015955*Math.pow((double)Humidity,(double )2))+(converted[0]-25)*(0.01+0.00008*(double)Humidity);
 
 		return converted;
+	}
+
+	private void sendCommand(String command) {
+
+		System.out.println("Turning lights " + command+'.');				
+		DigiMeshDevice myDevice = new DigiMeshDevice(PORT, BAUD_RATE);
+		//		byte[] dataToSend = DATA_TO_SEND.getBytes();
+		byte[] dataToSend = command.getBytes();
+
+		try {
+			myDevice.open();
+			// Obtain the remote XBee device from the XBee network.
+			XBeeNetwork xbeeNetwork = myDevice.getNetwork();
+			RemoteXBeeDevice remoteDevice = xbeeNetwork.discoverDevice(REMOTE_NODE_IDENTIFIER);
+			if (remoteDevice == null) {
+				System.out.println("Device: '" + REMOTE_NODE_IDENTIFIER + "' not found.");
+				System.exit(1);
+			}
+
+			System.out.format("Sending explicit data asynchronously to %s [%s - %s - %s - %s] >> %s | %s... ", 
+					remoteDevice.get64BitAddress(), 
+					HexUtils.integerToHexString(SOURCE_ENDPOINT, 1), 
+					HexUtils.integerToHexString(DESTINATION_ENDPOINT, 1), 
+					HexUtils.integerToHexString(CLUSTER_ID, 2),
+					HexUtils.integerToHexString(PROFILE_ID, 2), 
+					HexUtils.prettyHexString(HexUtils.byteArrayToHexString(dataToSend)), 
+					new String(dataToSend));
+
+			myDevice.sendExplicitDataAsync(remoteDevice, SOURCE_ENDPOINT,DESTINATION_ENDPOINT, 
+					CLUSTER_ID, PROFILE_ID, dataToSend);
+
+			System.out.println("Success");
+
+		} catch (XBeeException e) {
+			System.out.println("Error");
+			e.printStackTrace();
+			System.exit(1);
+		} finally {
+			myDevice.close();
+		}
 	}
 
 	private void showData(Message message) {
@@ -137,67 +180,52 @@ class Sense implements MessageListener {
 			//Calibrate sensor values
 			int[] taosCalcData = 
 					calculateTaos(tempMessage.get_VisLight_data(),tempMessage.get_InfLight_data());
-			
+
 			/** ####################### TEMPORARY CODE ####################### **/
-			
-			if(taosCalcData[0] <= 700) {
-				
-				System.out.println("Low environment luminosity, turning lights on.");
-				DigiMeshDevice myDevice = new DigiMeshDevice(PORT, BAUD_RATE);
-				byte[] dataToSend = DATA_TO_SEND.getBytes();
-				
-				try {
-					myDevice.open();
-					
-					// Obtain the remote XBee device from the XBee network.
-					XBeeNetwork xbeeNetwork = myDevice.getNetwork();
-					RemoteXBeeDevice remoteDevice = xbeeNetwork.discoverDevice(REMOTE_NODE_IDENTIFIER);
-					if (remoteDevice == null) {
-						System.out.println("Couldn't find the remote XBee device with '" + REMOTE_NODE_IDENTIFIER + "' Node Identifier.");
-						System.exit(1);
-					}
-					
-					System.out.format("Sending explicit data asynchronously to %s [%s - %s - %s - %s] >> %s | %s... ", 
-							remoteDevice.get64BitAddress(), 
-							HexUtils.integerToHexString(SOURCE_ENDPOINT, 1), 
-							HexUtils.integerToHexString(DESTINATION_ENDPOINT, 1), 
-							HexUtils.integerToHexString(CLUSTER_ID, 2), 
-							HexUtils.integerToHexString(PROFILE_ID, 2), 
-							HexUtils.prettyHexString(HexUtils.byteArrayToHexString(dataToSend)), 
-							new String(dataToSend));
-					
-					myDevice.sendExplicitDataAsync(remoteDevice, SOURCE_ENDPOINT, DESTINATION_ENDPOINT, CLUSTER_ID, PROFILE_ID, dataToSend);
-					
-					System.out.println("Success");
-					
-				} catch (XBeeException e) {
-					System.out.println("Error");
-					e.printStackTrace();
-					System.exit(1);
-				} finally {
-					myDevice.close();
+			if(environments[(tempMessage.get_nodeid() - 1)/ 5].compareTo("lab_16") == 0) {
+				System.out.println("This is env 16!");
+				//				Low environment luminosity
+				//				send a "turn on" lights command
+				//				to the Xbee network
+				if(taosCalcData[0] <= 10 && !lightsOn) {
+					sendCommand(DATA_TO_SEND);
+					lightsOn = true;
+					System.out.println("Lights: "+taosCalcData[0]+" Send on");
+				}
+				else if(taosCalcData[0] <= 10 && lightsOn) {
+					lightsOn = true;
+				}
+
+				//Turn on lights
+				else if(taosCalcData[0] > 10 && lightsOn) {
+					sendCommand(DATA_TO_SEND1);
+					System.out.println("Lights: "+taosCalcData[0]+" Send off");
+					lightsOn = false;
+				}
+				else if(taosCalcData[0] > 10 && !lightsOn) {
+					lightsOn = false;
 				}
 			}
-			
+
 			/** ########################################################################## **/
-			
+
 
 			double[] sensirionCalcData = 
 					calculateSensirion(tempMessage.get_Temp_data(),tempMessage.get_Hum_data());
 
 			int voltage = 
 					(1223 * 1024)/tempMessage.get_Voltage_data();
-			
+
 			if(voltage >= 2100) {
-				try {			
+				/**				try {			
 
 					//Set URL address
 					//192.168.200.242
 					URL url = new URL("http://192.168.200.242/api/v1/sensors");
-					
+
 					//Request connection
 					HttpURLConnection conn = (HttpURLConnection)url.openConnection();
-					
+
 					//Set request type
 					conn.setRequestMethod("POST");
 					//Set request properties
@@ -206,43 +234,43 @@ class Sense implements MessageListener {
 					conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
 					conn.setRequestProperty("charset", "UTF-8");
 					conn.setDoOutput(true);
-					
+
 					//Save sensor data to a formatted string					
 					String data = String.format("nodeID=%s&sensirion_temp=%s&"
-									+ "sensirion_hum=%s&intersema_temp=%s&"
-									+ "intersema_press=%s&infrared_light=%s&"
-									+ "light=%s&accel_x=%s&"
-									+ "accel_y=%s&voltage=%s&"								
-									+ "country=%s&state=%s&"
-									+ "city=%s&latitude=%s&"
-									+ "longitude=%s&env_id=%s&"
-									+ "date=%s", tempMessage.get_nodeid(), sensirionCalcData[0],
-									sensirionCalcData[1], tempMessage.getElement_Intersema_data(0)/10, 
-									tempMessage.getElement_Intersema_data(1)/10, taosCalcData[1], 
-									taosCalcData[0], tempMessage.get_AccelX_data(), 
-									tempMessage.get_AccelY_data(), voltage,
-									"Brazil", "Alagoas", "Maceió",
-									"-9.555032", "-35.774708", environments[(tempMessage.get_nodeid() - 1)/ 5], date);
-					
+							+ "sensirion_hum=%s&intersema_temp=%s&"
+							+ "intersema_press=%s&infrared_light=%s&"
+							+ "light=%s&accel_x=%s&"
+							+ "accel_y=%s&voltage=%s&"								
+							+ "country=%s&state=%s&"
+							+ "city=%s&latitude=%s&"
+							+ "longitude=%s&env_id=%s&"
+							+ "date=%s", tempMessage.get_nodeid(), sensirionCalcData[0],
+							sensirionCalcData[1], tempMessage.getElement_Intersema_data(0)/10, 
+							tempMessage.getElement_Intersema_data(1)/10, taosCalcData[1], 
+							taosCalcData[0], tempMessage.get_AccelX_data(), 
+							tempMessage.get_AccelY_data(), voltage,
+							"Brazil", "Alagoas", "Maceió",
+							"-9.555032", "-35.774708", environments[(tempMessage.get_nodeid() - 1)/ 5], date);
+
 
 					//Write data to server
 					DataOutputStream wr = new DataOutputStream(conn.getOutputStream());
 					wr.writeBytes(data);
 					wr.flush();
-					
+
 					//Close connection
 					wr.close();
 
 					int responseCode = conn.getResponseCode();					
 					System.out.println("\nSending 'POST' request to URL : " + url);
 					System.out.println("Response Code : " + responseCode);
-					
+
 
 				} catch (MalformedURLException e) {
 					System.out.println("A malformed URL exception has occurred"+ e.getMessage());
 				} catch (IOException e) {
 					System.out.println("Error:"+ e.getMessage());
-				}
+				}**/
 			}
 			else {
 				System.out.println("Voltage is too low, package rejected.\n");
